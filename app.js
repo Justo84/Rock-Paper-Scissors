02294bf83e4982b98de7681e646f31792cb8a18e
Justin Egan <justin.d.egan@gmail.com>
2023-02-15 10:46:33 -0500

    game logic basics

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = "ROCK"
+const PAPER = "PAPER"
+const SCISSORS = "SCISSORS"
+
+const getPlayerChoice = () => {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`, "").toUpperCase();
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+        alert("Invalid choice. Rock it is! ")
+    }
+}
 
 startGameBtn.addEventListener("click", () => {
     console.log("Game is starting...")
-})
+}) 
