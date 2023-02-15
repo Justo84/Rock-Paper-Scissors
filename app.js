426fdf53c795931eb96d1cb32f3ab6d7ccc886c6
Justin Egan <justin.d.egan@gmail.com>
2023-02-15 11:50:47 -0500

    random choice function

--- app.js
+++ app.js
@@ -3,17 +3,33 @@ const startGameBtn = document.getElementById('start-game-btn');
 const ROCK = "ROCK"
 const PAPER = "PAPER"
 const SCISSORS = "SCISSORS"
-const DEFAULT_CHOICE = ROCK
+const DEFAULT_CHOICE = getRandomChoice()
 
 let gameIsRunning = false;
 
 const getPlayerChoice = () => {
     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`, "").toUpperCase();
     if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
-        alert(`Invalid choice. ${DEFAULT_CHOICE} it is!`)
+        alert(`Invalid choice. You pick ${DEFAULT_CHOICE}!`)
         return DEFAULT_CHOICE
     }
     return selection
+}
+
+function getRandomChoice() {
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+    const randomValue = getRandomInt(3)
+    if (randomValue === 0) {
+        return ROCK
+    }
+    if (randomValue === 1) {
+        return PAPER
+    }
+    if (randomValue === 2) {
+        return SCISSORS
+    }
 }
 
 startGameBtn.addEventListener("click", () => {
@@ -22,6 +38,6 @@ startGameBtn.addEventListener("click", () => {
     }
     gameIsRunning = true
     console.log("Game is starting...")
-    const playerSelection = getPlayerChoice(); 
+    const playerSelection = getPlayerChoice();
     console.log(playerSelection)
 }) 
